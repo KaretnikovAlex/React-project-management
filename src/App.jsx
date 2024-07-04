12b13964984ee7c195484f6c7381878d5353ae40
KaretnikovAlex <shura.karetnikov.89@gmail.com>
2024-07-05 00:44:33 +0300

    collecting data from inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 import { NewProject } from "./components/NewProject";
 import { ProjectsSidebar } from "./components/ProjectsSidebar";
+import { NoProjectSelected } from "./components/NoProjectSelected"
+import { useState } from "react";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectState)
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>
+  } else {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
