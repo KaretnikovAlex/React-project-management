55660762e0d65b2d114c271518cf3e1916fb9099
KaretnikovAlex <shura.karetnikov.89@gmail.com>
2024-07-06 00:13:36 +0300

    save projects and derive in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -20,29 +20,32 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId
       }
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     })
   }
 
-  console.log(projectState)
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject}/>
+    content = <NewProject onAdd={handleAddProject} />
   } else {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects} />
       {content}
     </main>
   );
